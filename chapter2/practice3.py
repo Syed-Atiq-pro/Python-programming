0dc5dbfb1bbe600f5610409c8ebddf02814ca547
Atiq Syed <syedatiq4953@gmail.com>
2026-05-19 18:23:20 +0530

    Add files via upload

---
+++ chapter2/practice3.py
@@ -0,0 +1,3 @@
+a , b = int(input("Enter the number 1:")),int(input("Enter the number 2:"))
+print(a+b)
+print(type(a))
---
+++ chapter2/practice4.py
@@ -0,0 +1,2 @@
+a , b = int(input("Enter the number 1:")),int(input("Enter the number 2:"))
+print(a>b)
